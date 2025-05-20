b4326b7a1571ba2e0d73081478298864a702378a
FlavioCastr00 <154934234+FlavioCastr00@users.noreply.github.com>
2025-05-20 11:29:33 -0300

    add código PHP para conexão com DB (não testado)

---
+++ Desenvolvimento Web/assets/php/data-base-handler.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=filmesdb"; //Data Source Name (fonte dos dados)
+$dbusername = "root";
+$dbpassword = ""; //atribuir "root" caso não funcione
+
+try {
+    //PHP Data Objects:
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+
+    //Gerenciar erros
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Apagar essa linha caso não funcione
+}
+catch(PDOException $e) {
+    echo "Conexão com Banco de Dados falhou: " . $e->getMessage();
+}
